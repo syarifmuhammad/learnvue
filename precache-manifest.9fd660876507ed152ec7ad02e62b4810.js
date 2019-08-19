0bb20c376f0f6ab991037720e2ab11615f437665
syarifmuhammad <syarifmuhammad369@gmail.com>
2019-08-20 00:19:33 +0800

    set

---
+++ precache-manifest.9fd660876507ed152ec7ad02e62b4810.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/learnvue/robots.txt"
+  },
+  {
+    "revision": "38fe98d273b9c11553d5",
+    "url": "/learnvue/js/chunk-vendors.5de009e4.js"
+  },
+  {
+    "revision": "79d6b3114d770c5e07bf",
+    "url": "/learnvue/js/app.5e100b45.js"
+  },
+  {
+    "revision": "e4c5683cf2bb3601906c",
+    "url": "/learnvue/js/about.102914ac.js"
+  },
+  {
+    "revision": "7b1098814710066bde9b518293d39079",
+    "url": "/learnvue/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/learnvue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "38fe98d273b9c11553d5",
+    "url": "/learnvue/css/chunk-vendors.ed4dd371.css"
+  },
+  {
+    "revision": "79d6b3114d770c5e07bf",
+    "url": "/learnvue/css/app.2b1b5d93.css"
+  }
+];
